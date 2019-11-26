78f99443850fc72f4852531518bde33820745397
charlierobinson080@gmail.com <chr40@aber.ac.uk>
2019-11-26 16:30:33 +0000

    code

--- code/main.c
+++ code/main.c
@@ -30,6 +30,30 @@ void printMatrix(char *title, double *matrix); /* print a matrix */
 int computeSolution(void) {
     //need to find the sums for each row and column
 
+    //store the row and column sums here in an array
+    double *rowSumArray = malloc(sizeof(double) * size);
+    double *columnSumArray = malloc(sizeof(double) * size);
+
+    for (int col = 0; col < size; col++) {
+        double columnSum = 0;
+        for (int row = 0; row < size; row++)
+        {
+            columnSum += input[row * size + col];
+        }
+        printf("Sum for col %d = %f\n", col, columnSum);
+        columnSumArray[col] = columnSum;
+    }
+
+    for (int row = 0; row < size; row++)
+    {
+        double rowSum = 0;
+        for (int col = 0; col < size; col++)
+        {
+            rowSum += input[row * size + col];
+        }
+        printf("Sum for row %d = %f\n", row, rowSum);
+        rowSumArray[row] = rowSum;
+    }
 
     glp_prob *lp; /* the linear programming problem */
 
@@ -43,7 +67,7 @@ int computeSolution(void) {
     // which is 24
     int constraints = (size * size) + size + size; // size squared plus 2 lots of size
     glp_add_rows(lp, constraints);
-    
+
 
     return 0; /* This is not always correct, of course, and needs to be changed. */
 }
@@ -53,28 +77,28 @@ int computeSolution(void) {
 int main(int argc, char **argv) {
     int arg; /* used to run over the command line parameters */
 
-    if ( argc<2 ) { /* no command line parameter given */
+    if (argc < 2) { /* no command line parameter given */
         fprintf(stderr, "Usage: %s [file1] [file2] [file3] [...]\n"
                         "Where each [file] indicates the name of a file with an input.\n",
                 argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    if ( argv[1][0]=='-' && argv[1][1]=='d' && argv[1][2]==0 ) {
+    if (argv[1][0] == '-' && argv[1][1] == 'd' && argv[1][2] == 0) {
         /* If the first parameter is -d we activate debug mode. */
-        debug=1; /* switch debug mode on */
+        debug = 1; /* switch debug mode on */
         fprintf(stdout, "DEBUG: Debug mode activated\n"); /* tell us */
     } else {
-        debug=0; /* switch debug mode off */
+        debug = 0; /* switch debug mode off */
     }
 
-    for ( arg=1+debug; arg<argc; arg++ ) { /* go over remaining parameters */
-        if ( readInput(argv[arg]) ) { /* try to read file */
+    for (arg = 1 + debug; arg < argc; arg++) { /* go over remaining parameters */
+        if (readInput(argv[arg])) { /* try to read file */
             /* readInput returned with error message */
             fprintf(stderr, "%s: Cannot read input with filename %s. Skipping.\n",
                     argv[0], argv[arg]);
         } else {
-            if ( computeSolution() ) {
+            if (computeSolution()) {
                 fprintf(stdout, "%s: Input %s has a solution.\n",
                         argv[0], argv[arg]);
                 checkSolution();
@@ -94,67 +118,67 @@ int main(int argc, char **argv) {
 
 /* The following function prints a matrix including the row and column sums */
 void printMatrix(char *title, double *matrix) {
-    int	i, j; /* looping over rows and columns */
+    int i, j; /* looping over rows and columns */
     double sum; /* to compute the sum */
 
     fprintf(stdout, "%s:\n", title);
     /* print the matrix and compute the row sums on the fly */
-    for ( i=0; i<size; i++ ) {
-        for ( j=0, sum=0.0; j<size; j++ ) {
-            sum += matrix[i*size+j];
-            fprintf(stdout,"%8.2f ", matrix[i*size+j]);
+    for (i = 0; i < size; i++) {
+        for (j = 0, sum = 0.0; j < size; j++) {
+            sum += matrix[i * size + j];
+            fprintf(stdout, "%8.2f ", matrix[i * size + j]);
         }
         fprintf(stdout, "(row sum: %8.2f)\n", sum);
     }
     /* print separating line */
-    for ( j=0; j<size; j++ ) {
+    for (j = 0; j < size; j++) {
         fprintf(stdout, "---------");
     }
-    fprintf(stdout,"\n");
+    fprintf(stdout, "\n");
     /* now compute the column sums and print them */
-    for ( j=0; j<size; j++ ) { /* we consistently use j for columns */
-        for ( i=0, sum=0.0; i<size; i++ ) {
-            sum += matrix[i*size+j];
+    for (j = 0; j < size; j++) { /* we consistently use j for columns */
+        for (i = 0, sum = 0.0; i < size; i++) {
+            sum += matrix[i * size + j];
         }
-        fprintf(stdout,"%8.2f ", sum);
+        fprintf(stdout, "%8.2f ", sum);
     }
-    fprintf(stdout,"(column sums)\n");
+    fprintf(stdout, "(column sums)\n");
 }
 
 /* The following function checks of a solution is valid. */
 void checkSolution(void) {
-    int	i, j; /* to run over the arrays */
+    int i, j; /* to run over the arrays */
     double sum1, sum2; /* to compute the sums over the rows and columns */
 
     /* check rows and that all numbers are integers and rounded */
-    for ( i=0; i<size; i++ ) {
-        for ( j=0, sum1=sum2=0.0; j<size; j++ ) {
-            sum1 += input[i*size+j];
-            sum2 += solution[i*size+j];
-            if ( myFloor(solution[i*size+j]) != solution[i*size+j] ) {
+    for (i = 0; i < size; i++) {
+        for (j = 0, sum1 = sum2 = 0.0; j < size; j++) {
+            sum1 += input[i * size + j];
+            sum2 += solution[i * size + j];
+            if (myFloor(solution[i * size + j]) != solution[i * size + j]) {
                 fprintf(stdout, "Error: %lf is not an integer (%d/%d).\n",
-                        solution[i*size+j], i, j);
+                        solution[i * size + j], i, j);
                 return;
             }
-            if ( (myFloor(input[i*size+j])!=solution[i*size+j]) &&
-                 (myCeil(input[i*size+j])!=solution[i*size+j]) ) {
+            if ((myFloor(input[i * size + j]) != solution[i * size + j]) &&
+                (myCeil(input[i * size + j]) != solution[i * size + j])) {
                 fprintf(stdout, "Error: %lf is not rounded from %lf (%d/%d).\n",
-                        solution[i*size+j], input[i*size+j], i, j);
+                        solution[i * size + j], input[i * size + j], i, j);
                 return;
             }
         }
-        if ( (myFloor(sum1)!=sum2) && (myCeil(sum1)!=sum2) ) {
+        if ((myFloor(sum1) != sum2) && (myCeil(sum1) != sum2)) {
             fprintf(stdout, "Error: Row sum for row %d not valid.\n", i);
             return;
         }
     }
     /* check columns*/
-    for ( j=0; j<size; j++ ) {
-        for ( i=0, sum1=sum2=0.0; i<size; i++ ) {
-            sum1 += input[i*size+j];
-            sum2 += solution[i*size+j];
+    for (j = 0; j < size; j++) {
+        for (i = 0, sum1 = sum2 = 0.0; i < size; i++) {
+            sum1 += input[i * size + j];
+            sum2 += solution[i * size + j];
         }
-        if ( (myFloor(sum1)!=sum2) && (myCeil(sum1)!=sum2) ) {
+        if ((myFloor(sum1) != sum2) && (myCeil(sum1) != sum2)) {
             fprintf(stdout, "Error: Row sum for row %d not valid.\n", i);
             return;
         }
@@ -167,54 +191,54 @@ int readInput(char *filename) {
     double value; /* value read from input file */
 
     /* try to open the file for reading */
-    if ( ( fh = fopen(filename, "rt") ) == NULL ) {
-        if ( debug ) {
+    if ((fh = fopen(filename, "rt")) == NULL) {
+        if (debug) {
             fprintf(stdout, "DEBUG: Unable to open file %s for reading.\n",
                     filename);
         }
         return 1; /* unable to open file, flag failure */
     }
     /* read the first integer, the number of columns/rows */
-    if ( fscanf(fh, "%d", &size)!= 1) {
-        if ( debug ) {
+    if (fscanf(fh, "%d", &size) != 1) {
+        if (debug) {
             fprintf(stdout, "DEBUG: Unable to read input size.\n");
         }
         fclose(fh); /* close file to avoid ununsed open files */
         return 1; /* flag failure */
     }
 
-    if ( size<2 ) {
-        if ( debug ) {
+    if (size < 2) {
+        if (debug) {
             fprintf(stdout, "DEBUG: Received %d as input size.\n", size);
         }
         fclose(fh); /* close file to avoid unused open files */
         return 1; /* flag failure */
     }
     /* allocate the memory for the input */
-    if ( ( input = (double *)malloc(sizeof(double)*size*size) ) == NULL ) {
-        if ( debug ) {
+    if ((input = (double *) malloc(sizeof(double) * size * size)) == NULL) {
+        if (debug) {
             fprintf(stdout, "DEBUG: Unable to allocate %ld bytes.\n",
-                    sizeof(int)*size*size);
+                    sizeof(int) * size * size);
         }
         fclose(fh); /* close file to avoid unused open files */
         return 1; /* flag failure */
     }
     /* allocate the memory for the solution */
-    if ( ( solution = (double *)malloc(sizeof(double)*size*size) ) == NULL ) {
-        if ( debug ) {
+    if ((solution = (double *) malloc(sizeof(double) * size * size)) == NULL) {
+        if (debug) {
             fprintf(stdout, "DEBUG: Unable to allocate %ld bytes.\n",
-                    sizeof(int)*size*size);
+                    sizeof(int) * size * size);
         }
         fclose(fh); /* close file to avoid unused open files */
         return 1; /* flag failure */
     }
 
     /* read the actual values */
-    for ( i=0; i<size; i++ ) {
-        for ( j=0; j<size; j++ ) {
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
             /* attempt to read next value */
-            if ( fscanf(fh, "%lf", &value)!= 1) {
-                if ( debug ) {
+            if (fscanf(fh, "%lf", &value) != 1) {
+                if (debug) {
                     fprintf(stdout, "DEBUG: Unable to read input value (%d/%d).\n",
                             i, j);
                 }
@@ -224,25 +248,25 @@ int readInput(char *filename) {
                 fclose(fh);
                 return 1; /* flag failure */
             }
-            input[i*size+j]=value;
+            input[i * size + j] = value;
         }
     }
-    if ( debug ) {
-        fprintf(stdout,"Read the following input with size %d x %d\n", size, size);
-        for ( i=0; i<size; i++ ) {
-            for ( j=0; j<size; j++ ) {
-                fprintf(stdout,"%5.2f ", input[i*size+j]);
+    if (debug) {
+        fprintf(stdout, "Read the following input with size %d x %d\n", size, size);
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                fprintf(stdout, "%5.2f ", input[i * size + j]);
             }
-            fprintf(stdout,"\n");
+            fprintf(stdout, "\n");
         }
     }
     return 0; /* all okay */
 }
 
 double myFloor(double x) {
-    return floor(x+EPSILON);
+    return floor(x + EPSILON);
 }
 
 double myCeil(double x) {
-    return ceil(x-EPSILON);
+    return ceil(x - EPSILON);
 }
